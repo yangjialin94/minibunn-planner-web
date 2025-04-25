554da894a762a9f06d7cec90b3c2ebe887f0634c
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-04-25 16:10:20 -0400

    moved firebase config vars to env file

--- src/auth/firebaseClient.ts
+++ src/auth/firebaseClient.ts
@@ -1,19 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
+
+// Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  NEXT_PUBLIC_FIREBASE_API_KEY,
+  NEXT_PUBLIC_FIREBASE_APP_ID,
+  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+} from "@/env";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAigPvMs_F1Ye8RDEpJYXeoU4Wygm6N6EQ",
-  authDomain: "minimal-todo-637a2.firebaseapp.com",
-  projectId: "minimal-todo-637a2",
-  storageBucket: "minimal-todo-637a2.firebasestorage.app",
-  messagingSenderId: "548960895885",
-  appId: "1:548960895885:web:a36b7e16bc0929e95833c4",
-  measurementId: "G-GN1RKEJL7Z",
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
