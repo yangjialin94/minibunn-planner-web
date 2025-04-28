c994019bb3a38dd233e117c8016f800bccc4cbfe
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-04-28 11:41:33 -0400

    added logo nav to marketing website

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { motion } from "motion/react";
 import Image from "next/image";
+import Link from "next/link";
 import React, { memo, useEffect, useState } from "react";
 
 import { formatDateLocal } from "@/utils/date";
+
+const MotionLink = motion(Link);
 
 const TimeDisplay = memo(() => {
   const [currentLocalTime, setCurrentLocalTime] = useState(() =>
@@ -26,12 +30,24 @@ TimeDisplay.displayName = "TimeDisplay";
 function Header() {
   return (
     <header>
-      <Image
-        src="/minibunn-planner-logo.svg"
-        alt="logo"
-        width={100}
-        height={100}
-      />
+      {/* Logo */}
+      <MotionLink
+        href="https://www.minibunnplanner.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-4"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <Image
+          src="/minibunn-planner-logo.svg"
+          alt="logo"
+          width={100}
+          height={100}
+        />
+      </MotionLink>
+
+      {/* Time Display */}
       <TimeDisplay />
     </header>
   );
