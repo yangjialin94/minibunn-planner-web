50b1c86866e7c46009a14d320cec399d70d062ea
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-07-11 11:36:04 -0400

    syntax twick

--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -5,14 +5,18 @@ type UserState = {
   name: string;
   email: string;
   isSubscribed: boolean;
+};
 
+interface UserActions {
   setName: (name: string) => void;
   setEmail: (email: string) => void;
   setIsSubscribed: (isSubscribed: boolean) => void;
   clearStore: () => void;
-};
+}
 
-export const useUserStore = create<UserState>()(
+type UserStore = UserState & UserActions;
+
+export const useUserStore = create<UserStore>()(
   persist(
     (set) => ({
       name: "",
